13fbad5ba14b635c8b8a4286c6a754e657c766a1
syedfaraz <32864042+SyedFaraz55@users.noreply.github.com>
2019-08-31 19:56:52 +0530

    updated http.js

--- DOM/http.js
+++ DOM/http.js
@@ -13,3 +13,34 @@ request.addEventListener('readystatechange',(e) => {
 request.open('GET','http://puzzle.mead.io/puzzle');
 request.send();
 
+// API Request, XMLHttpRequest(),fetchAPI
+
+const request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  if(this.readyState ==4 && this.status == 200){
+    const data = JSON.parse(this.responseText);
+    const rdata = data.last;
+    console.log("last: " + rdata);
+  }
+}
+request.open("GET","https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD");
+request.send();
+
+fetch("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD")
+.then(response=>response.json())
+.then(data=>console.log(data.last));
+
+async function fetchPrice(url){
+  const fetchResult = fetch(url);
+  const response = await fetchResult;
+  if(response.ok){
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } else {
+    throw Error(response.statusText)
+  }
+}
+
+fetchPrice("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD");
+
+
